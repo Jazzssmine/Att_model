f1cf46e7d3c8f9c9a8ad813d9e2b6b6c024332d3
Jass <44516315+Jazzssmine@users.noreply.github.com>
2021-06-02 20:49:14 +0800

    transformer layer

---
+++ trans_layer.py
@@ -0,0 +1,230 @@
+import tensorflow as tf
+
+
+# create tf.data.Dataset
+def get_dataset_from_csv(csv_file_path, shuffle=False, batch_size=128):
+    def process(features):
+        movie_ids_string = features["sequence_movie_ids"]
+        sequence_movie_ids = tf.strings.split(movie_ids_string, ",").to_tensor()
+
+        # The last movie id in the sequence is the target movie.
+        features["target_movie_id"] = sequence_movie_ids[:, -1]
+        features["sequence_movie_ids"] = sequence_movie_ids[:, :-1]
+
+        ratings_string = features["sequence_ratings"]
+        sequence_ratings = tf.strings.to_number(
+            tf.strings.split(ratings_string, ","), tf.dtypes.float32
+        ).to_tensor()
+
+        # The last rating in the sequence is the target for the model to predict.
+        target = sequence_ratings[:, -1]
+        features["sequence_ratings"] = sequence_ratings[:, :-1]
+
+        return features, target
+
+    dataset = tf.data.experimental.make_csv_dataset(
+        csv_file_path,
+        batch_size=batch_size,
+        column_names=CSV_HEADER,
+        num_epochs=1,
+        header=False,
+        field_delim="|",
+        shuffle=shuffle,
+    ).map(process)
+
+    return dataset
+
+
+# create model inputs
+
+def create_model_inputs():
+    return {
+        "user_id": layers.Input(name="user_id", shape=(1,), dtype=tf.string),
+        "sequence_movie_ids": layers.Input(
+            name="sequence_movie_ids", shape=(sequence_length - 1,), dtype=tf.string
+        ),
+        "target_movie_id": layers.Input(
+            name="target_movie_id", shape=(1,), dtype=tf.string
+        ),
+        "sequence_ratings": layers.Input(
+            name="sequence_ratings", shape=(sequence_length - 1,), dtype=tf.float32
+        ),
+        "sex": layers.Input(name="sex", shape=(1,), dtype=tf.string),
+        "age_group": layers.Input(name="age_group", shape=(1,), dtype=tf.string),
+        "occupation": layers.Input(name="occupation", shape=(1,), dtype=tf.string),
+    }
+
+
+def encode_input_features(
+    inputs,
+    include_user_id=True,
+    include_user_features=True,
+    include_movie_features=True,
+):
+
+    encoded_transformer_features = []
+    encoded_other_features = []
+
+    other_feature_names = []
+    if include_user_id:
+        other_feature_names.append("user_id")
+    if include_user_features:
+        other_feature_names.extend(USER_FEATURES)
+
+    ## Encode user features
+    for feature_name in other_feature_names:
+        # Convert the string input values into integer indices.
+        vocabulary = CATEGORICAL_FEATURES_WITH_VOCABULARY[feature_name]
+        idx = StringLookup(vocabulary=vocabulary, mask_token=None, num_oov_indices=0)(
+            inputs[feature_name]
+        )
+        # Compute embedding dimensions
+        embedding_dims = int(math.sqrt(len(vocabulary)))
+        # Create an embedding layer with the specified dimensions.
+        embedding_encoder = layers.Embedding(
+            input_dim=len(vocabulary),
+            output_dim=embedding_dims,
+            name=f"{feature_name}_embedding",
+        )
+        # Convert the index values to embedding representations.
+        encoded_other_features.append(embedding_encoder(idx))
+
+    ## Create a single embedding vector for the user features
+    if len(encoded_other_features) > 1:
+        encoded_other_features = layers.concatenate(encoded_other_features)
+    elif len(encoded_other_features) == 1:
+        encoded_other_features = encoded_other_features[0]
+    else:
+        encoded_other_features = None
+
+    ## Create a movie embedding encoder
+    movie_vocabulary = CATEGORICAL_FEATURES_WITH_VOCABULARY["movie_id"]
+    movie_embedding_dims = int(math.sqrt(len(movie_vocabulary)))
+    # Create a lookup to convert string values to integer indices.
+    movie_index_lookup = StringLookup(
+        vocabulary=movie_vocabulary,
+        mask_token=None,
+        num_oov_indices=0,
+        name="movie_index_lookup",
+    )
+    # Create an embedding layer with the specified dimensions.
+    movie_embedding_encoder = layers.Embedding(
+        input_dim=len(movie_vocabulary),
+        output_dim=movie_embedding_dims,
+        name=f"movie_embedding",
+    )
+    # Create a vector lookup for movie genres.
+    genre_vectors = movies[genres].to_numpy()
+    movie_genres_lookup = layers.Embedding(
+        input_dim=genre_vectors.shape[0],
+        output_dim=genre_vectors.shape[1],
+        embeddings_initializer=tf.keras.initializers.Constant(genre_vectors),
+        trainable=False,
+        name="genres_vector",
+    )
+    # Create a processing layer for genres.
+    movie_embedding_processor = layers.Dense(
+        units=movie_embedding_dims,
+        activation="relu",
+        name="process_movie_embedding_with_genres",
+    )
+
+    ## Define a function to encode a given movie id.
+    def encode_movie(movie_id):
+        # Convert the string input values into integer indices.
+        movie_idx = movie_index_lookup(movie_id)
+        movie_embedding = movie_embedding_encoder(movie_idx)
+        encoded_movie = movie_embedding
+        if include_movie_features:
+            movie_genres_vector = movie_genres_lookup(movie_idx)
+            encoded_movie = movie_embedding_processor(
+                layers.concatenate([movie_embedding, movie_genres_vector])
+            )
+        return encoded_movie
+
+    ## Encoding target_movie_id
+    target_movie_id = inputs["target_movie_id"]
+    encoded_target_movie = encode_movie(target_movie_id)
+
+    ## Encoding sequence movie_ids.
+    sequence_movies_ids = inputs["sequence_movie_ids"]
+    encoded_sequence_movies = encode_movie(sequence_movies_ids)
+    # Create positional embedding.
+    position_embedding_encoder = layers.Embedding(
+        input_dim=sequence_length,
+        output_dim=movie_embedding_dims,
+        name="position_embedding",
+    )
+    positions = tf.range(start=0, limit=sequence_length - 1, delta=1)
+    encodded_positions = position_embedding_encoder(positions)
+    # Retrieve sequence ratings to incorporate them into the encoding of the movie.
+    sequence_ratings = tf.expand_dims(inputs["sequence_ratings"], -1)
+    # Add the positional encoding to the movie encodings and multiply them by rating.
+    encoded_sequence_movies_with_poistion_and_rating = layers.Multiply()(
+        [(encoded_sequence_movies + encodded_positions), sequence_ratings]
+    )
+
+    # Construct the transformer inputs.
+    for encoded_movie in tf.unstack(
+        encoded_sequence_movies_with_poistion_and_rating, axis=1
+    ):
+        encoded_transformer_features.append(tf.expand_dims(encoded_movie, 1))
+    encoded_transformer_features.append(encoded_target_movie)
+
+    encoded_transformer_features = layers.concatenate(
+        encoded_transformer_features, axis=1
+    )
+
+    return encoded_transformer_features, encoded_other_features
+
+
+# create model
+
+include_user_id = False
+include_user_features = False
+include_movie_features = False
+
+hidden_units = [256, 128]
+dropout_rate = 0.1
+num_heads = 3
+
+
+def create_model():
+    inputs = create_model_inputs()
+    transformer_features, other_features = encode_input_features(
+        inputs, include_user_id, include_user_features, include_movie_features
+    )
+
+    # Create a multi-headed attention layer.
+    attention_output = layers.MultiHeadAttention(
+        num_heads=num_heads, key_dim=transformer_features.shape[2], dropout=dropout_rate
+    )(transformer_features, transformer_features)
+
+    # Transformer block.
+    attention_output = layers.Dropout(dropout_rate)(attention_output)
+    x1 = layers.Add()([transformer_features, attention_output])
+    x1 = layers.LayerNormalization()(x1)
+    x2 = layers.LeakyReLU()(x1)
+    x2 = layers.Dense(units=x2.shape[-1])(x2)
+    x2 = layers.Dropout(dropout_rate)(x2)
+    transformer_features = layers.Add()([x1, x2])
+    transformer_features = layers.LayerNormalization()(transformer_features)
+    features = layers.Flatten()(transformer_features)
+
+    # Included the other features.
+    if other_features is not None:
+        features = layers.concatenate(
+            [features, layers.Reshape([other_features.shape[-1]])(other_features)]
+        )
+
+    # Fully-connected layers.
+    for num_units in hidden_units:
+        features = layers.Dense(num_units)(features)
+        features = layers.BatchNormalization()(features)
+        features = layers.LeakyReLU()(features)
+        features = layers.Dropout(dropout_rate)(features)
+
+    outputs = layers.Dense(units=1)(features)
+    model = keras.Model(inputs=inputs, outputs=outputs)
+    return model
+
